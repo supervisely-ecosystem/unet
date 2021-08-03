563ba434e46e5ae3a6cf92abd9c4693d37787bbe
max kolomeychenko <maxim.kolomeychenko@gmail.com>
2021-08-03 15:31:23 +0300

    start dataset - wip

--- models/seg_dataset.py
+++ models/seg_dataset.py
@@ -4,9 +4,19 @@ from PIL import Image
 
 
 class SlySegDataset(Dataset):
-    def __init__(self, count, transform=None):
+    def __init__(self, size, sly_augs=None):
         #self.input_images, self.target_masks = simulation.generate_random_data(192, 192, count=count)
-        self.transform = transform
+        self.sly_augs = sly_augs
+
+        self.transforms_img = transforms.Compose([
+            transforms.Resize(size=size, interpolation=Image.NEAREST),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),  # imagenet
+        ])
+        self.transforms_ann = transforms.Compose([
+            transforms.Resize(size=size, interpolation=Image.NEAREST),
+            transforms.ToTensor(),
+        ])
 
     def __len__(self):
         return len(self.input_images)
@@ -14,17 +24,12 @@ class SlySegDataset(Dataset):
     def __getitem__(self, idx):
         image = self.input_images[idx]
         mask = self.target_masks[idx]
-        if self.transform:
-            image = self.transform(image)
+
+        if self.sly_augs:
+            image, mask = self.sly_augs(image, mask)
+
+        image = self.transforms_img(image)
+        mask = self.transforms_ann(image)
 
         return [image, mask]
 
-
-def get_dataset(size=256):
-    trans = transforms.Compose([
-        transforms.Resize(size=size, interpolation=Image.NEAREST),
-        # augmentations here
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),  # imagenet
-    ])
-    return SlySegDataset()
