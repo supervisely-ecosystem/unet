9f81dbed46c4376c5169ef6592214407184aa088
cxnt <pavelbartsits@gmail.com>
2024-06-06 17:13:20 +0400

    update train

--- supervisely/train/src/sly_globals.py
+++ supervisely/train/src/sly_globals.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import sys
 import supervisely as sly
 from supervisely.app.v1.app_service import AppService
+from supervisely.nn.checkpoints.unet import UNetCheckpoint
 
 root_source_dir = str(Path(sys.argv[0]).parents[3])
 print(f"Root source directory: {root_source_dir}")
@@ -37,6 +38,8 @@ task_id = my_app.task_id
 team_id = int(os.environ['context.teamId'])
 workspace_id = int(os.environ['context.workspaceId'])
 project_id = int(os.environ['modal.state.slyProjectId'])
+
+checkpoint = UNetCheckpoint(team_id)
 
 project_info = api.project.get_info_by_id(project_id)
 if project_info is None:  # for debug
