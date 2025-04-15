b8d2a6832ffd058673fcff5917e20e322ae02e5c
cxnt <pavelbartsits@gmail.com>
2025-04-15 16:26:21 +0400

    fix deploy params

--- supervisely/train/src/ui/step07_train.py
+++ supervisely/train/src/ui/step07_train.py
@@ -348,7 +348,7 @@ def run_benchmark(api: sly.Api, task_id, classes, state, remote_dir):
                 checkpoint_name=best_filename,
                 checkpoint_url=checkpoint_path,
             )
-            m.load_on_device(deploy_params)
+            m.load_on_device(**deploy_params)
             asyncio.set_event_loop(asyncio.new_event_loop())
             m.serve()
 
