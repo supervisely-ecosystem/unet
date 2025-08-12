53e14f948fae70c385e590d24b1985e843f792a0
cxnt <pavelbartsits@gmail.com>
2025-08-12 15:39:51 +0000

    Improve error handling in experiment creation within step07_train.py by consolidating warning message formatting.

--- supervisely/train/src/ui/step07_train.py
+++ supervisely/train/src/ui/step07_train.py
@@ -603,14 +603,12 @@ def train(api: sly.Api, task_id, context, state, app_logger):
         sly.logger.info("Creating experiment info")
         create_experiment(state["selectedModel"], remote_dir, report_id, eval_metrics, primary_metric_name)
     except Exception as e:
-        sly.logger.warning(
-            f"Couldn't create experiment, this training session will not appear in experiments table. Error: {e}"
-        )
-    
+        sly.logger.error(f"Couldn't create experiment, this training session will not appear in the experiments table. Error: {e}")
+
     w.workflow_input(api, g.project_info, state)
     w.workflow_output(api, g.sly_unet_generated_metadata, state, benchmark_report_template)
 
-    
+
     # stop application
     # g.my_app.show_modal_window("Training is finished, app is still running and you can preview predictions dynamics over time."
     #                           "Please stop app manually once you are finished with it.")
